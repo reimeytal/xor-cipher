ccb8ea0354ba2693f9408d0b3605e3e145f9fc60
reimeytal <rei12.meytal11@gmail.com>
2021-01-11 00:00:24 +0200

    Created algorithm

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # xor-cipher
 A simple XOR cipher
+
+Argument 1: -c
+Argument 2: File name
+Argument 3: Encryption/decryption key
--- xor-cipher.asm
+++ xor-cipher.asm
@@ -1,18 +1,85 @@
 global _start
 
 extern strcmp
-
 section .data
-s1 db "1234", 0x0
-s2 db "1234", 0x0
+code                db "-c", 0x0
+invalid_args_string db "Invalid argument(s). Refer to 'https://github.com/reimeytal/xor-cipher' for help.", 0xA
+len_invalid_args equ $ - invalid_args_string
 
-section .text:
+section .text
 _start:
-  mov eax, s1
-  mov ebx, s2
+  pop eax
+  cmp eax, 1
+  je invalid_args
 
+  add esp, 4
+  pop ebx
+
+  mov eax, code
   call strcmp
+  cmp eax, 1
+  je code_func
 
-  mov ebx, eax
+  jmp invalid_args
+
+code_func:
+  pop ebx
+
+  mov eax, 5
+  mov ecx, 0
+  int 0x80
+
+  push eax
+  jmp code_loop
+
+code_loop:
+  mov eax, 3
+  pop ebx
+  sub esp, 8
+  mov ecx, esp
+  add esp, 8
+  mov edx, 4
+  int 0x80
+
+  mov ebp, eax
+
+  mov ebx, [esp]
+  mov ecx, [ecx]
+  xor ecx, [ebx]
+  sub esp, 8
+  mov [esp], ecx
+  mov ecx, esp
+  add esp, 8
+
+  mov edx, ebp
+  mov eax, 4
+  mov ebx, 1
+  int 0x80
+
+  sub esp, 4
+  cmp ebp, 4
+  jne close_exit
+  jmp code_loop
+  jmp exit
+
+invalid_args:
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, invalid_args_string
+  mov edx, len_invalid_args
+  int 0x80
+
   mov eax, 1
+  mov ebx, 2
+  int 0x80
+
+close_exit:
+  pop ebx
+  mov eax, 6
+  int 0x80
+  jmp exit
+
+exit:
+  mov eax, 1
+  mov ebx, 0
   int 0x80
